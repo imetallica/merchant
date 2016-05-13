142b242a77226ded645ab424808aae7ce6efe5f7
Iuri L. Machado <imetallica@hotmail.com>
2016-05-13 20:09:39 -0300

    Updated docs.

--- lib/merchant/credit_card/master_card.ex
+++ lib/merchant/credit_card/master_card.ex
@@ -1,4 +1,7 @@
 defmodule Merchant.CreditCard.MasterCard do
+  @moduledoc """
+  The implementation of the CreditCard protocol for MasterCard.
+  """
   @derive Merchant.CreditCard
   defstruct [number: :nil, check_digit: :nil, regex: ~r/^5[1-5][0-9]{14}$/]
 end
--- lib/merchant/credit_card/visa.ex
+++ lib/merchant/credit_card/visa.ex
@@ -1,4 +1,7 @@
 defmodule Merchant.CreditCard.Visa do
+  @moduledoc """
+  The implementation of the CreditCard protocol for Visa.
+  """
   @derive Merchant.CreditCard
   defstruct [number: :nil, check_digit: :nil, regex: ~r/^4[0-9]{12}(?:[0-9]{3})?$/]
 
